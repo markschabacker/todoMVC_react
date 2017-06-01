37fb498b9e97d07a41dc2a010d2c2550c2cbe2ca
Mark Schabacker <mark.schabacker@agilethought.com>
2017-06-01 15:28:55 -0400

    Implement todo editing

--- src/Todo.ts
+++ src/Todo.ts
@@ -1,7 +1,9 @@
 export class Todo {
     public completed: boolean;
+    public editing: boolean;
 
     constructor(public id: number, public text: string) {
         this.completed = false;
+        this.editing = false;
     }
 }
