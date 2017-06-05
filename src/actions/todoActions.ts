d9b3e882858d20b11ebbc7cf678bfe13565be2fd
Mark Schabacker <mark.schabacker@agilethought.com>
2017-06-05 16:08:00 -0400

    Strongly type actions

--- src/actions/todoActions.ts
+++ src/actions/todoActions.ts
@@ -1,21 +1,42 @@
 import { Todo } from '../Todo';
 
-export function addTodo(todo: Todo) {
-    return { type: 'ADD_TODO', todo};
+export let ADD_TODO: string = 'ADD_TODO';
+export let SET_COMPLETION: string = 'SET_COMPLETION';
+export let SET_TEXT: string = 'SET_TEXT';
+export let REMOVE: string = 'REMOVE';
+export let REMOVE_COMPLETED: string = 'REMOVE_COMPLETED';
+
+export interface ITodoAction<T> {
+    type: string;
+    payload: T;
 }
 
-export function setTodoCompletion(todoState: { id: number, completed: boolean}) {
-    return { type: 'SET_COMPLETION', todoState};
+export interface IAddTodoAction extends ITodoAction<{ todo: Todo }> { }
+
+export function addTodo(todo: Todo): IAddTodoAction {
+    return { type: ADD_TODO, payload: { todo }};
 }
 
-export function updateTodoText(todoState: { id: number, text: string}) {
-    return { type: 'SET_TEXT', todoState};
+export interface ISetTodoCompletionAction extends ITodoAction<{ todo: { id: number, completed: boolean }}> {}
+
+export function setTodoCompletion(todo: { id: number, completed: boolean}): ISetTodoCompletionAction {
+    return { type: SET_COMPLETION, payload: { todo }};
 }
 
-export function removeTodo(id: number) {
-    return { type: 'REMOVE', id};
+export interface IUpdateTodoTextAction extends ITodoAction<{ todo: { id: number, text: string }}> {}
+
+export function updateTodoText(todo: { id: number, text: string}): IUpdateTodoTextAction {
+    return { type: SET_TEXT, payload: { todo }};
 }
 
-export function removeCompleted() {
-    return { type: 'REMOVE_COMPLETED' };
+export interface IRemoveTodoAction extends ITodoAction<{ todo: { id: number }}> {}
+
+export function removeTodo(id: number): IRemoveTodoAction {
+    return { type: REMOVE, payload: { todo: { id }}};
+}
+
+export interface IRemoveCompletedTodosAction extends ITodoAction<{}> {}
+
+export function removeCompleted(): IRemoveCompletedTodosAction {
+    return { type: REMOVE_COMPLETED, payload: {}};
 }
