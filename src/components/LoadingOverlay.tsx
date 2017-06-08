6a3ef64af4478ea1e1f9350af2bfe338dd093e37
Mark Schabacker <mark.schabacker@agilethought.com>
2017-06-08 08:06:37 -0400

    Convert loading overlay styling to CSS

--- src/components/LoadingOverlay.tsx
+++ src/components/LoadingOverlay.tsx
@@ -4,31 +4,16 @@ interface ILoadingOverlayProps {
     visible: boolean;
 }
 
-/* tslint:disable:object-literal-sort-keys */
-const overlayStyle: React.CSSProperties = {
-    position: 'fixed',
-    height: '100%',
-    width: '100%',
-    top: '0',
-    left: '0',
-    background: 'rgba(0, 0, 0, 0.7)',
-    zIndex: 1000,
-};
-/* tslint:enable */
-
 export class LoadingOverlay extends React.Component<ILoadingOverlayProps, {}> {
 
     public render(): JSX.Element | null {
         return (
-            <div style={this.getStyle()}>
-                {this.props.children}
+            <div className={this.getClassName()}>
             </div>
         );
     }
 
-    private getStyle(): React.CSSProperties {
-        return this.props.visible
-            ? overlayStyle
-            : {};
+    private getClassName(): string {
+        return `loading-overlay-${this.props.visible ? 'visible' : 'hidden'}`;
     }
 }
